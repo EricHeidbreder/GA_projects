ddef37d67ab21b2483433158500b86b364f5fa0b
eheidbreder <ewheidbreder@gmail.com>
2020-08-05 23:24:30 -0500

    Got my score down to 24000!

--- custom_funcs/custom_funcs.py
+++ custom_funcs/custom_funcs.py
@@ -1,4 +1,3 @@
 def category_to_bool_cols(dataframe, column):
     dummy_split = pd.get_dummies(dataframe[column], column) # Creates dummy columns with the name {column}_{value_in_row} per get_dummies documentation
-    for dummy_key in dummy_split: # Iterates through dummy_key in dummy_split
-        dataframe[dummy_key] = dummy_split[dummy_key] # adds new columns named {dummy_key} to original dataframe
+    dataframe = df.join(other = dummy_split) # Iterates through dummy_key in dummy_split
